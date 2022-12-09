47782827583e4bd4b5be3644dfaf23c3415c76f3
Jose Mendoza <joseMendoza9696>
2022-12-09 12:28:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Installation Steps
+THIS PROJECTS WORKS WITH NODEv16.15.1
+
+## Using npm
+
+Run commands
+
+1) ```npm install```
+
+
+2) ```npm run dev```
+
+
+## Or using yarn
+
+Run commands 
+
+1) ```npm install --global yarn```
+
+2) ```yarn install```
+
+3) ```yarn run dev```
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Header from "../components/Headers";
+import Banner from "../components/Banner";
+import ProductsFeed from "../components/ProductsFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100" >
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head> 
+      {/* HEADER COMPONENT */}
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* BANNER */}
+        <Banner />
+
+        {/* PRODUCT FEED */}
+        <ProductsFeed products={products} />
+
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  );
+
+  return { props: {
+    products
+  }}
+}
+
