917302564f637b7cd4a346f312abdec9c53d23af
Jose Mendoza <joseMendoza9696>
2022-12-13 23:26:45 -0400

    using nest backend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5d3f5ad306d38adbf08"; }
+/******/ 		__webpack_require__.h = function() { return "2e03bd90259be78b2a71"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
